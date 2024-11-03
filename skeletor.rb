479d20c74760a26c782a73e048d505be7a0e29cc
Jason Nathan <jjnathanjr@gmail.com>
2024-11-03 23:26:25 +0800

    updated sha256

--- skeletor.rb
+++ skeletor.rb
@@ -2,7 +2,7 @@ class Skeletor < Formula
   desc "Your project description here"
   homepage "https://github.com/jasonnathan/skeletor"
   url "https://github.com/jasonnathan/skeletor/releases/download/v2.0.1/skeletor.tar.gz" # link to your tar.gz binary
-  sha256 "169bb439a7e69d08967f891cbbaf2e6537ceec9f9eb736ebbb503c9bca83366c"  # Replace this with the actual SHA256 checksum of your tar.gz file
+  sha256 "5ce7b4047a317fef8f424850e13b736efd32f38a7b3de59843647813d24a4a69"  # Replace this with the actual SHA256 checksum of your tar.gz file
   license "MIT"
 
   def install
